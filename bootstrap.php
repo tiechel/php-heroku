cb238540753145416f636827458f429ecc5e087f
tiechel <alicetype.luna@gmail.com>
2018-08-07 22:02:48 +0900

    $app生成をbootstrap.phpに移動

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,28 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+use App\ControllerResolver;
+
 $dotenv = new Dotenv\Dotenv(__DIR__);
 if (file_exists('.env')) {
     $dotenv->load(__DIR__);
 }
+
+$app = new Silex\Application();
+$app['debug'] = true;
+$app['resolver'] = new ControllerResolver();
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), [
+    'monolog.logfile' => 'php://stderr',
+]);
+
+// Our web handlers
+$app->get('/', function() use($app) {
+    $app['monolog']->addDebug('logging output');
+    return 'hello root world';
+});
+
+$app->get('/webhook', 'App\Controller\WebhookController::get');
+
+$app->run();
--- web/index.php
+++ web/index.php
@@ -1,26 +1,3 @@
 <?php
 
 require __DIR__.'/../bootstrap.php';
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use App\ControllerResolver;
-
-$app = new Silex\Application();
-$app['debug'] = true;
-$app['resolver'] = new ControllerResolver();
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), [
-    'monolog.logfile' => 'php://stderr',
-]);
-
-// Our web handlers
-$app->get('/', function() use($app) {
-    $app['monolog']->addDebug('logging output');
-    return 'hello root world';
-});
-
-$app->get('/webhook', 'App\Controller\WebhookController::get');
-
-$app->run();
